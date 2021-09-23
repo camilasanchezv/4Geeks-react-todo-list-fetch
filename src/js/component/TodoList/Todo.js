f037b6fd6a4d6034d3dc464ea92ecf44afd07caa
Camila <camilasanchezvinik@hotmail.com>
2021-09-22 21:50:33 -0300

    exercise

--- src/js/component/TodoList/Todo.js
+++ src/js/component/TodoList/Todo.js
@@ -3,9 +3,36 @@ import { any, number, string } from "prop-types";
 
 import "./styles.scss";
 
-export default function Todo({ todo, id, list, setList }) {
-	const handleDelete = () => {
-		setList(list.filter((element, index) => index !== id));
+export default function Todo({ todo, id, list, setList, clean }) {
+	const handleDelete = async () => {
+		if (list.length === 1) {
+			clean();
+		} else {
+			let auxList = list.filter((element, index) => index !== id);
+
+			const url =
+				"https://assets.breatheco.de/apis/fake/todos/user/camila";
+			fetch(url, {
+				method: "PUT",
+				body: JSON.stringify(auxList),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+				.then()
+				.then(response => {
+					{
+						if (response.ok) setList(auxList);
+						else
+							alert(
+								"Oops! We cant delete the task by the moment."
+							);
+					}
+				})
+				.catch(() =>
+					alert("Oops! We cant delete the task by the moment.")
+				);
+		}
 	};
 
 	return (
@@ -22,5 +49,6 @@ Todo.propTypes = {
 	todo: string,
 	id: number,
 	list: any,
-	setList: any
+	setList: any,
+	clean: any
 };
--- src/js/component/TodoList/TodoList.js
+++ src/js/component/TodoList/TodoList.js
@@ -5,43 +5,113 @@ import "./styles.scss";
 
 export default function TodoList() {
 	const [list, setList] = useState([]);
-	const [newTodo, setNewTodo] = useState("");
-	const [length, setLength] = useState(0);
+	const [newTodo, setNewTodo] = useState();
+	const url = "https://assets.breatheco.de/apis/fake/todos/user/camila";
 
-	const addTodo = () => {
+	const addTodo = async () => {
 		if (newTodo) {
-			setList([...list, newTodo]);
-			setNewTodo("");
+			const todo = {
+				label: newTodo,
+				done: false
+			};
+
+			fetch(url, {
+				method: "PUT",
+				body: JSON.stringify([...list, todo]),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+				.then()
+				.then(response => {
+					if (response.ok) {
+						setList([...list, todo]);
+						setNewTodo("");
+					} else alert("Oops! We cant add the task by the moment.");
+				})
+				.catch(() =>
+					alert("Oops! We cant add the task by the moment.")
+				);
 		}
 	};
 
+	const getList = async () => {
+		let response = await (
+			await fetch(url, {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+		).json();
+		setList(response);
+	};
+
+	const create = async () => {
+		await fetch(url, {
+			method: "POST",
+			body: JSON.stringify([]),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		});
+	};
+
+	const clean = async () => {
+		fetch(url, {
+			method: "DELETE",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then()
+			.then(response => {
+				{
+					if (response.ok) {
+						setList([]);
+						create();
+					} else
+						alert(
+							"Oops! We cant delete the task list by the moment."
+						);
+				}
+			})
+			.catch(() =>
+				alert("Oops! We cant delete the task list by the moment.")
+			);
+	};
+
 	useEffect(() => {
-		setLength(list.length);
-	}, [list]);
+		getList();
+	}, []);
 
 	return (
-		<div className="todo-list">
-			<input
-				className="input"
-				value={newTodo}
-				onChange={e => setNewTodo(e.target.value)}
-				placeholder="write here..."
-			/>
-			<button onClick={addTodo} className="add">
-				+
-			</button>
-			<div className="list">
-				{list.map((todo, index) => (
-					<Todo
-						key={index + todo}
-						id={index}
-						todo={todo}
-						list={list}
-						setList={setList}
-					/>
-				))}
+		<>
+			<div className="todo-list">
+				<input
+					className="input"
+					value={newTodo}
+					onChange={e => setNewTodo(e.target.value)}
+					placeholder="write here..."
+				/>
+				<button onClick={addTodo} className="add">
+					+
+				</button>
+				<div className="list">
+					{list.map((todo, index) => (
+						<Todo
+							key={index + todo}
+							id={index}
+							todo={todo.label}
+							list={list}
+							setList={setList}
+							clean={clean}
+						/>
+					))}
+				</div>
+				<p>{list.length}</p>
 			</div>
-			<p>{length}</p>
-		</div>
+			<button onClick={clean}>clean</button>
+		</>
 	);
 }
